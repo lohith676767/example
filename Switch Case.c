65ba5521f14ac618d9f90d1ecf93e2e447a9541d
lohith676767 <lohithkaran15@gmail.com>
2026-05-10 19:02:23 +0530

    Create Switch Case.c

---
+++ Switch Case.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    char c;
+    printf("input character: ");
+    scanf("%c",&c);
+    switch(c){
+        case 'a':
+        case 'A':
+        case 'e':
+        case 'E':
+        case 'i':
+        case 'I':
+        case 'o':
+        case 'O':
+        case 'u':
+        case 'U':
+        printf("%c is a vowel",c);
+        break;
+        default :
+        printf("%c is a consonant",c);
+        }
+}
